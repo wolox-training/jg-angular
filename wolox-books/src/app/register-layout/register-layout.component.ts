b513f7cc87b5ff4ed462e8532b5225ac2c067161
Juan <juanmartin.gutierrez@wolox.com.ar>
2020-04-30 15:47:03 -0300

    Add changes to form
    I made some ident changes, like deleting empty lines and add empty lines in eof. But the most important is that we made this form with ngFor that makes all more DRY <3

--- wolox-books/src/app/register-layout/register-layout.component.ts
+++ wolox-books/src/app/register-layout/register-layout.component.ts
@@ -6,7 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-layout.component.scss']
 })
 export class RegisterLayoutComponent implements OnInit {
-
+  
+  fields = [
+    {
+      name: 'Nombre',
+      type: 'text',
+      id: 'name'
+    },
+    {
+      name: 'Apellido',
+      type: 'text',
+      id: 'last-name'
+    },
+    {
+      name: 'Email',
+      type: 'text',
+      id: 'email'
+    },
+    {
+      name: 'Password',
+      type: 'text',
+      id: 'password'
+    },
+    {
+      name: 'Confirmación de Password',
+      type: 'text',
+      id: 'confirm-pass'
+    }
+  ] 
   constructor() { }
 
   ngOnInit(): void {
